3f069cdb2baa5b1f959626e2280a8d5bf8bdcdd8
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-12-14 20:20:00 +0545

    fixfactorial

--- lab-1/factorial.c
+++ lab-1/factorial.c
@@ -3,7 +3,8 @@
 int fact(int i) { return (i == 0) ? (1) : i * fact(i - 1); }
 
 int main() {
-  int n printf("enter number to find factorial ");
+  int n;
+  printf("enter number to find factorial ");
   scanf("%d", &n);
   printf("factorial id: %d\n", fact(n));
 }
